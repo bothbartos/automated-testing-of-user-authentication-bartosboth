230ab19af6936fdf1f4ebdfef20199e9e0f08133
bartosboth <bothbartos@gmail.com>
2025-06-30 11:44:03 +0200

    created dashboardPage

---
+++ src/test/java/pages/DashboardPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class DashboardPage extends BasePage{
+    @FindBy(id = "flash")
+    private WebElement alert;
+    @FindBy(id = "username")
+    private WebElement welcomeMessageWithUsername;
+    @FindBy(css = "a[href='/logout']")
+    private WebElement logoutButton;
+
+    public DashboardPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getWelcomeMessage() {
+        waitForElementToBeVisible(welcomeMessageWithUsername);
+        return welcomeMessageWithUsername.getText();
+    }
+
+    public void clickLogout() {
+        waitForElementToBeClickable(logoutButton);
+        logoutButton.click();
+    }
+
+    public boolean isSecureAreaDisplayed() {
+        return getCurrentUrl().contains("secure");
+    }
+
+    public void verifyDashboardIsDisplayed() {
+        assertThat(getCurrentUrl().contains("Secure Area"));
+    }
+}
