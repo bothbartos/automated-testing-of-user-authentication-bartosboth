d47d28100ab7c48db9159b8f33b6a8593669e8c1
bartosboth <bothbartos@gmail.com>
2025-06-30 10:18:13 +0200

    created DriverManager

---
+++ src/test/java/utils/DriverManager.java
@@ -0,0 +1,52 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class DriverManager {
+    private static final ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
+
+    public static void setDriver(String browser) {
+        WebDriver driver = createDriver(browser);
+        driverThreadLocal.set(driver);
+    }
+
+    public static WebDriver getDriver() {
+        return driverThreadLocal.get();
+    }
+
+    public static void quitDriver() {
+        WebDriver driver = driverThreadLocal.get();
+        if (driver != null) {
+            driver.quit();
+            driverThreadLocal.remove();
+        }
+    }
+
+    private static WebDriver createDriver(String browser) {
+        return switch (browser.toLowerCase()){
+            case "chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                options.addArguments("--disable-blink-features=AutomationControlled");
+                options.addArguments("--disable-extensions");
+                yield new ChromeDriver(options);
+            }
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                yield new FirefoxDriver();
+            }
+            case "edge" -> {
+                WebDriverManager.edgedriver().setup();
+                yield new EdgeDriver();
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + browser);
+        };
+    }
+}
