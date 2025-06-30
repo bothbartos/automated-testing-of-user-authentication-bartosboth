c0b4ae44b627e9f65d38282b2b99bcce03084d33
bartosboth <bothbartos@gmail.com>
2025-06-30 18:57:42 +0200

    deleted unused method from testDataReader

--- src/test/java/utils/TestDataReader.java
+++ src/test/java/utils/TestDataReader.java
@@ -18,38 +18,6 @@ public class TestDataReader {
     private static final String TEST_DATA_PATH = "/test-data/";
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static List<User> readUsersFromCSV(String fileName) {
-        List<User> users = new ArrayList<>();
-
-        try (InputStream inputStream = TestDataReader.class.getResourceAsStream(TEST_DATA_PATH + fileName)) {
-            CSVFormat format = CSVFormat.Builder.create()
-                    .setHeader()
-                    .setSkipHeaderRecord(true)
-                    .build();
-
-            CSVParser csvParser = CSVParser.parse(inputStream, StandardCharsets.UTF_8, format);
-
-            for (CSVRecord record : csvParser) {
-                User user = new User();
-                user.setUsername(record.get("username"));
-                user.setPassword(record.get("password"));
-                users.add(user);
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to read CSV file: " + fileName, e);
-        }
-
-        return users;
-    }
-
-    public static User getUserByUsername(String fileName, String username) {
-        return readUsersFromCSV(fileName).stream()
-                .filter(user -> username.equals(user.getUsername()))
-                .findFirst()
-                .orElse(null);
-    }
-
     public static List<User> readUsersFromJSON(String fileName) {
         try (InputStream inputStream = TestDataReader.class.getResourceAsStream(TEST_DATA_PATH + fileName)) {
             JavaType listType = objectMapper.getTypeFactory()
